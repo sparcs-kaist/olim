24e6d37108bc01b69d2f64014862bebd1e980fee
Jong-Hyuk, Jung <heretor92@gmail.com>
2014-03-18 06:42:12 +0900

    Make a model 'Filesys'. (not yet, because of uploader field)

--- olim/olim/apps/storage/models.py
+++ olim/olim/apps/storage/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Filesys(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+    date = models.DateField(auto_now=True)
+    #uploader = models.ForeignKey('account.User')
+    thumbnail = models.FileField(upload_to='thumb')
+
+    parent_dir = models.CharField(max_length=100)
+    is_dir = models.BooleanField()
+
+    def __str__(self):
+        if is_dir:
+            return '/' + name
+        else:
+            return name
