a727160166e88de7553e1f9c7d11947f615d7db0
Jong-Hyuk, Jung <heretor92@gmail.com>
2014-03-21 07:52:20 +0900

    Refs #7: Made back-end file listing view.

--- olim/olim/apps/storage/views.py
+++ olim/olim/apps/storage/views.py
@@ -7,23 +7,22 @@ from olim.apps.storage.models import Filesys
 
 def filesys(request):
     url = request.META["PATH_INFO"]
-    this_dir_name = filter(None, url.split("/"))[-1]
-    this_dir = Filesys.objects.filter(name=this_dir_name)[0]
+    this_url_name = filter(None, url.split("/"))[-1]
+    this_url = Filesys.objects.filter(name=this_url_name)[0]
     user_auth = request.user.is_authenticated()
 
     url_check = ""
 
+    url_list = filter(None, url.split("/"))
+    path_list = []
     dir_contents = []
     file_contents = []
     is_child = False
+    parent_path = ""
 
-    if this_dir_name != "root":
-        is_child = True
+    # For secured directories and URL check
 
-    # For secured directories
-    print user_auth
-
-    if this_dir.is_secured:
+    if this_url.is_secured:
         if user_auth:
             url_check = _checkURL(url)
         else:
@@ -31,22 +30,34 @@ def filesys(request):
     else:
         url_check = _checkURL(url)
 
+    # For quick path
+
+    for i, item in enumerate(url_list):
+        path = ""
+        for j in range(i+1):
+            path += "/" + url_list[j]
+        path_list.append({'url':url_list[i], 'path':path})
+
+    if this_url_name != "root":
+        is_child = True
+        parent_path = path_list[-2]
+
     # Make content lists shown to users
 
     if url_check:
-        this_list = Filesys.objects.filter(parent_dir=this_dir_name)
+        this_list = Filesys.objects.filter(parent_dir=this_url_name).order_by('name')
         for item in this_list:
             if item.is_secured:
-                print "SECURED"
                 if user_auth:
                     if item.is_dir:
                         dir_contents.append({
-                            'name': item.name,
+                            'name': "/"+item.name,
                             'url': item.url,
                             'date': item.date,
                             'uploader': item.uploader,
                             'thumbnail': item.thumbnail,
                             'is_secured': item.is_secured,
+                            'format': item.format
                         })
                     else:
                         file_contents.append({
@@ -56,16 +67,18 @@ def filesys(request):
                             'uploader': item.uploader,
                             'thumbnail': item.thumbnail,
                             'is_secured': item.is_secured,
+                            'format': item.format
                         })
             else:
                 if item.is_dir:
                     dir_contents.append({
-                        'name': item.name,
+                        'name': "/"+item.name,
                         'url': item.url,
                         'date': item.date,
                         'uploader': item.uploader,
                         'thumbnail': item.thumbnail,
                         'is_secured': item.is_secured,
+                        'format': item.format
                     })
                 else:
                     file_contents.append({
@@ -75,15 +88,20 @@ def filesys(request):
                         'uploader': item.uploader,
                         'thumbnail': item.thumbnail,
                         'is_secured': item.is_secured,
+                        'format': item.format
                     })
     else:
         return HttpResponse("FALSE URL")
 
-    print dir_contents
-    print file_contents
-    print is_child
-
-    return HttpResponse(url)
+    return render_to_response('list.html', {
+        'dir_contents' : dir_contents,
+        'file_contents' : file_contents,
+        'is_child' : is_child,
+        'parent_path' : parent_path,
+        'this_path' : url,
+        'this_url' : this_url_name,
+        'path_list' : path_list
+    }, context_instance=RequestContext(request))
 
 def _checkURL(url):
     url_comp = filter(None, url.split("/"))
