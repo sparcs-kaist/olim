75482fcc781f6be2c11cfb2bd13013eacca80ea1
Jong-Hyuk, Jung <heretor92@gmail.com>
2014-04-06 03:45:28 +0900

    Refs #20: Changed filesys models.py @Filesys.url : +max_length=200, +unique, +db_index @Filesys.uploader : +null, blank +Filesys.file : +FileField @Filesys.parent_dir : -CharField, +IntegerField

--- olim/olim/apps/storage/models.py
+++ olim/olim/apps/storage/models.py
@@ -2,15 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import admin
 from django.core.exceptions import ValidationError
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+
+fs = FileSystemStorage(location=settings.PROJECT_DIR, base_url='/')
 
 class Filesys(models.Model):
     name = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
-    format = models.CharField(max_length=20)
+    url = models.CharField(max_length=200, unique=True, db_index=True)
+    format = models.CharField(max_length=10)
     date = models.DateField(auto_now=True)
-    uploader = models.ForeignKey(User)
+    uploader = models.ForeignKey(User, null=True, blank=True)
+    file = models.FileField(storage=fs, upload_to='files', null=True, blank=True)
     thumbnail = models.FileField(upload_to='thumbs', null=True, blank=True)
-    parent_dir = models.CharField(max_length=100, null=True, blank=True)
+    parent_dir = models.IntegerField(null=True, blank=True)
     is_dir = models.BooleanField(default=False)
     is_secured = models.BooleanField(default=False)
 
